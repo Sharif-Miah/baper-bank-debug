5adadc7c2beb50df9edffa6bcbad2d754c7ff202
farhan sharif <farhansharif1410@gmail.com>
2022-09-10 16:38:59 -0230

    withdraw plm fixed

--- js/deposit.js
+++ js/deposit.js
@@ -6,21 +6,21 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     3. convert string value to a number
     */
   const newDepositAmount = getInputFieldValueById("deposit-field");
-  // console.log(newDepositAmount);
+
   /* 
     1. get previous deposit total by id
 
     */
   const previousDepositTotal = getTextElementValueById("deposit-total");
 
-  // calculate new deposit total
+  // // calculate new deposit total
   const newDepositTotal = previousDepositTotal + newDepositAmount;
-  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  // console.log(newDepositTotal);
   // set deposit total value
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
 });
